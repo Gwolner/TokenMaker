43e5ef4553b457409d0832ad76d5a44894eb6348
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2020-03-14 21:48:28 -0300

    Sugestao de melhoria

--- gerador-token.php
+++ gerador-token.php
@@ -1,34 +1,23 @@
 <?php
 
-$token = NULL; //Token indefinido.
-$digitos = 8; //Define quantos digitos o token possui.
-$flagError = false; //Controla a chamada da mensagem de erro.
-
-for($i=0;$i < $digitos;$i++){
-    $sensibilidade = rand(0,9); //Define a sensibilidade de alternar LETRA e NUMERO.
-
-    if(!$flagError){
-        if($sensibilidade < 3){
+function token($digitos)
+{
+    $token = "";
+    while (strlen($token) < $digitos) {
+        $sensibilidade = rand(0, 9); //Define a sensibilidade de alternar LETRA e NUMERO.
+        if ($sensibilidade < 3) {
             //Sorteia letras maiusculas.
-            $token .= chr(rand(65,90)); 
-
-        }else if($sensibilidade >= 3 && $sensibilidade <= 5){
+            $token .= chr(rand(65, 90));
+        } else if ($sensibilidade >= 3 && $sensibilidade <= 5) {
             //Sorteia letras minusculas.
-            $token .= chr(rand(97,122));
-
-        }else if($sensibilidade >= 6 && $sensibilidade <9){
+            $token .= chr(rand(97, 122));
+        } else if ($sensibilidade >= 6 && $sensibilidade < 9) {
             //Sorteia numeros de um único dígito.
-            $token .= rand(0,9);
-
-        } else {
-            //Divergência da faixa de sensibilidade e do valor gerado.
-            $token = "Erro: Verifique a faixa de sensibilidade";
-            $flagError = true;
+            $token .= rand(0, 9);
         }
     }
+    return $token;
 }
-
-echo $token."<br/>"; //Exibe o token.
+$token = token(8);
+echo $token . "<br/>"; //Exibe o token.
 echo strlen($token); //Confere o tamanho do token.
-
-?>
